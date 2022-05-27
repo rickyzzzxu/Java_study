ab01f27f7018cf8d4793dabd5ceb65ad518583f7
rickyzzzxu <rickyzzzz@163.com>
2022-05-27 20:09:10 +0800

    spring_aop注解和xml

--- spring/spring_aop/src/main/java/com/ricky/anno/MyAspect.java
+++ spring/spring_aop/src/main/java/com/ricky/anno/MyAspect.java
@@ -22,22 +22,22 @@ public class MyAspect {
 
     @Around("execution(* com.ricky.anno.*.*(..))")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        System.out.println("环绕前增强.....");
+        System.out.println("环绕前增强...");
         Object proceed = pjp.proceed();
-        System.out.println("环绕后增强.....");
+        System.out.println("环绕后增强...");
         return proceed;
     }
 
     @AfterThrowing("execution(* com.ricky.anno.*.*(..))")
     public void afterThrowing(){
-        System.out.println("异常抛出增强..........");
+        System.out.println("异常抛出增强........");
     }
 
     //@After("execution(* com.ricky.anno.*.*(..))")
     //@After("pointcut()")
     @After("MyAspect.pointcut()")
     public void after(){
-        System.out.println("最终增强..........");
+        System.out.println("最终增强.......");
     }
 
     // 定义切点表达式
