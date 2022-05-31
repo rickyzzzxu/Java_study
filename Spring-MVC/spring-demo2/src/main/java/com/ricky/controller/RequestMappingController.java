a6d5c9ba0bd6f660d517417fb8fb0e30e506a0ab
rickyzzzxu <rickyzzzz@163.com>
2022-05-31 23:36:17 +0800

    @RequestMapping注解详解

--- Spring-MVC/spring-demo2/src/main/java/com/ricky/controller/RequestMappingController.java
+++ Spring-MVC/spring-demo2/src/main/java/com/ricky/controller/RequestMappingController.java
@@ -2,6 +2,7 @@ package com.ricky.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,6 +29,22 @@ public class RequestMappingController {
             headers = {"Host=localhost:9000"}
     )
     public String testParamsAndHeaders() {
+        return "success";
+    }
+
+    //@RequestMapping("/a?a/testAnt")
+    //@RequestMapping("/a*a/testAnt")
+    @RequestMapping("/**/testAnt")
+    public String testAnt() {
+        return "success";
+    }
+
+    @RequestMapping("/testPath/{id}/{username}")
+    public String testPath(@PathVariable("id") Integer id, @PathVariable("username") String username) {
+
+        System.out.println("id:" + id);
+        System.out.println("username:" + username);
+
         return "success";
     }
 
