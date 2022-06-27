146935135d3c531200546304661f6bdd85073b2f
rickyzzzxu <rickyzzzz@163.com>
2022-06-27 15:18:40 +0800

    bean注解管理方式和注解配置文件

---
+++ Spring5/spring-demo3/src/main/java/com/ricky/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.ricky.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ricky")
+public class SpringConfig {
+}
--- Spring5/spring-demo3/src/test/java/TestDemo.java
+++ Spring5/spring-demo3/src/test/java/TestDemo.java
@@ -1,6 +1,9 @@
+import com.ricky.config.SpringConfig;
 import com.ricky.service.UserService;
+import com.sun.xml.internal.fastinfoset.vocab.Vocabulary;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestDemo {
@@ -8,6 +11,14 @@ public class TestDemo {
     @Test
     public void demo1() {
         ApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        UserService userService = ac.getBean(UserService.class);
+        System.out.println(userService);
+        userService.add();
+    }
+
+    @Test
+    public void demo2() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserService userService = ac.getBean(UserService.class);
         System.out.println(userService);
         userService.add();
