14970871dbdb01e14d32bb229d97e1c789c3ac63
rickyzzzxu <rickyzzzz@163.com>
2022-07-13 15:02:39 +0800

    update files

---
+++ mybatis/demo2/src/test/java/com/ricky/mybatis/test/ParameterMapperTest.java
@@ -0,0 +1,79 @@
+package com.ricky.mybatis.test;
+
+import com.ricky.mybatis.mapper.ParameterMapper;
+import com.ricky.mybatis.pojo.User;
+import com.ricky.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ParameterMapperTest {
+
+    @Test
+    public void testCheckLoginByParam(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        User user = mapper.checkLoginByParam("admin", "123456");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        int result = mapper.insertUser(new User(null, "李四", "123", 23, "男", "123@qq.com"));
+        System.out.println(result);
+    }
+
+    @Test
+    public void testCheckLoginByMap(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "admin");
+        map.put("password", "123456");
+        User user = mapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLogin(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        User user = mapper.checkLogin("admin", "123456");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetUserByUsername(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        User user = mapper.getUserByUsername("admin");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAllUser(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        List<User> list = mapper.getAllUser();
+        list.forEach(user -> System.out.println(user));
+    }
+
+    @Test
+    public void testJDBC() throws Exception {
+        String username = "admin";
+        Class.forName("");
+        Connection connection = DriverManager.getConnection("", "", "");
+        //PreparedStatement ps = connection.prepareStatement("select * from t_user where username = '" + username + "'");
+        PreparedStatement ps = connection.prepareStatement("selelt * from t_user where username = ?");
+        ps.setString(1, username);
+    }
+}
---
+++ mybatis/demo2/src/test/java/com/ricky/mybatis/test/SelectMapperTest.java
@@ -0,0 +1,46 @@
+package com.ricky.mybatis.test;
+
+import com.ricky.mybatis.mapper.SelectMapper;
+import com.ricky.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+
+public class SelectMapperTest {
+
+    @Test
+    public void testGetAllUserToMap(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        System.out.println(mapper.getAllUserToMap());
+    }
+
+    @Test
+    public void testGetUserByIdToMap(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        System.out.println(mapper.getUserByIdToMap(3));
+    }
+
+    @Test
+    public void testGetCount(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        System.out.println(mapper.getCount());
+    }
+
+    @Test
+    public void testGetAllUser(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        System.out.println(mapper.getAllUser());
+    }
+
+    @Test
+    public void testGetUserById(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        System.out.println(mapper.getUserById(3));
+    }
+
+}
