b28112d109ae548a1f51f15d7afb3a10852d1c1a
rickyzzzxu <rickyzzzz@163.com>
2022-09-26 17:16:48 +0800

    jedis操作

---
+++ redis/Jedis_demo/src/main/java/com/ricky/jedis/JedisDemo1.java
@@ -0,0 +1,37 @@
+package com.ricky.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class JedisDemo1 {
+
+    public static void main(String[] args) {
+
+        Jedis jedis = new Jedis("192.168.34.128", 6379);
+
+        // redis密码
+        jedis.auth("xdg112233");
+
+        String ping = jedis.ping();
+
+        System.out.println(ping);
+
+    }
+
+    @Test
+    public void demo2() {
+        //创建Jedis对象
+        Jedis jedis = new Jedis("192.168.34.128", 6379);
+        jedis.auth("xdg112233");
+
+        jedis.sadd("names", "lucy");
+        jedis.sadd("names", "mary");
+
+        Set<String> names = jedis.smembers("names");
+        System.out.println(names);
+        jedis.close();
+    }
+
+}
